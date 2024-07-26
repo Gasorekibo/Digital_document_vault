455f082820aa69662e6181cf46f5732e7befbc3e
Gasorekibo <m.gasore@alustudent.com>
2024-07-26 14:55:28 +0200

    setup and initialize frontend user interface of the project

---
+++ frontend/digitaldocvault/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-white mx-8 font-inter">Digital Doc application</h1>
+    </div>
+  );
+}
+
+export default App;
